56898127be5fed071dfbcabf0103e1222c49e96e
eormskim <eorms6599@daum.net>
2023-06-07 22:11:27 +0900

    작업중

--- src/main/java/com/example/springboot/SpringConfig.java
+++ src/main/java/com/example/springboot/SpringConfig.java
@@ -3,11 +3,21 @@ package com.example.springboot;
 import com.example.springboot.repository.MemberRepository;
 import com.example.springboot.repository.MemoryMemberRepository;
 import com.example.springboot.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource){
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService(){
@@ -16,6 +26,6 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+        return new MemoryMemberRepository(dataSource);
     }
 }
