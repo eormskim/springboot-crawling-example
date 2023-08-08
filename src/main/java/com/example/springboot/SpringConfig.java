e0ebd3cad64482aa35ca64b6d967bf9e749481ff
eormskim <eorms6599@daum.net>
2023-08-08 19:34:57 +0900

    크롤링관련 config 추가

--- src/main/java/com/example/springboot/SpringConfig.java
+++ src/main/java/com/example/springboot/SpringConfig.java
@@ -1,8 +1,9 @@
 package com.example.springboot;
 
 import com.example.springboot.aop.TimeTraceAop;
-import com.example.springboot.repository.JpaMemberRepository;
+import com.example.springboot.repository.CrawlerRepository;
 import com.example.springboot.repository.MemberRepository;
+import com.example.springboot.service.CrawlerService;
 import com.example.springboot.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -12,10 +13,12 @@ import org.springframework.context.annotation.Configuration;
 public class SpringConfig {
 
     private final MemberRepository memberRepository;
+    private final CrawlerRepository crawlerRepository;
 
     @Autowired
-    public SpringConfig(MemberRepository memberRepository) {
+    public SpringConfig(MemberRepository memberRepository, CrawlerRepository crawlerRepository) {
         this.memberRepository = memberRepository;
+        this.crawlerRepository = crawlerRepository;
     }
 
     /*private EntityManager em;
@@ -35,6 +38,10 @@ public class SpringConfig {
     @Bean
     public MemberService memberService(){
         return new MemberService(memberRepository);
+    }
+    @Bean
+    public CrawlerService crawlerService(){
+        return new CrawlerService(crawlerRepository);
     }
 
     @Bean
